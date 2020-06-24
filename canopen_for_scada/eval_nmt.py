70a72e124e181f1a46d00ea734f5bc0b9d3daa20
nekomata <jazzpiano1004@gmail.com>
2020-06-24 19:19:58 +0700

    canopen_for_scada/eval_nmt : [Update] update for a better comment and OS selection is added

--- canopen_for_scada/eval_nmt.py
+++ canopen_for_scada/eval_nmt.py
@@ -14,11 +14,23 @@ from canopen_for_scada import *
 
 
 if __name__ == '__main__':
+
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-o", "--os", help="OS of your machine (linux, win)", action="store", type=str, default="win")
+    args = vars(ap.parse_args())
+
+    user_os = args["os"]
     
     # Create CANOpen Network
     mycanopen_network = Canopen_Network_SCADA()
-    mycanopen_network.connect(bustype='usb2can', channel='69E696BD', bitrate=125000)
-    
+    if user_os == "linux":
+    	mycanopen_network.connect(bustype='socketcan', channel='can0')
+    	print("connect via \'socketcan\' to can0")
+    elif user_os == "win":
+    	mycanopen_network.connect(bustype='usb2can', channel='69E696BD', bitrate=125000)
+    	print("connect via \'usb2can\' to channel 69E696BD, bitrate=125000")
+   
     # Create some CANOpen device
     mydevice = Canopen_Device_SCADA(node_id=1, object_dictionary='LC5100.eds')
     
